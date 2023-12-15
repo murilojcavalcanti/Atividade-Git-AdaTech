670c06dfd0d6f1ddfc36f461530bfdc4d0702d4e
Pedr0Siqueira <pedro_jsiqueira@hotmail.com>
2023-12-15 09:39:49 -0300

    fixed

--- Atividade_Git/Program.cs
+++ Atividade_Git/Program.cs
@@ -1,5 +1,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Commit Murilo");
 Console.WriteLine("Commit Herlon");
-Console.WriteLine("Commit Pedro");
 Console.WriteLine("Commit Pedro2");
